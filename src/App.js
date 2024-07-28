06148626bcd00acd301960f0bdd067fe5149239c
saranyajs2727 <saranyac2727@gmail.com>
2024-07-28 18:49:06 +0530

    data updtaed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+// import React from "react";
+// import Dashboard from "./components/Dashborad/Dashboard";
+// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import Graph from "./components/Graph/Graph";
+// import CommonQuestion from "./components/commonQuestion/CommonQuestion";
+// import Table from "./components/Table/Table";
+// import InsightWorkSpace from "./components/InsightWorkSpace/InsightWorkSpace";
+// import UploadFile from "./components/UploadFile/UploadFile";
+
+// const App = () => {
+//   return (
+//     // <div>
+//     //     //   <GlobalStyle />
+//     //     //   <Dashboard />
+
+//     //     // </div>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Dashboard />}>
+//           <Route index element={<InsightWorkSpace />} />
+//           <Route path="commonquestions" element={<CommonQuestion />} />
+//           <Route path="table" element={<Table />} />
+//           <Route path="/graph" element={<Graph />} />
+//           <Route path="/uploadfile" element={<UploadFile />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 import React from "react";
 import Dashboard from "./components/Dashborad/Dashboard";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Graph from "./components/Graph/Graph";
 import CommonQuestion from "./components/commonQuestion/CommonQuestion";
 import Table from "./components/Table/Table";
@@ -9,19 +40,14 @@ import UploadFile from "./components/UploadFile/UploadFile";
 
 const App = () => {
   return (
-    // <div>
-    //     //   <GlobalStyle />
-    //     //   <Dashboard />
-
-    //     // </div>
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />}>
           <Route index element={<InsightWorkSpace />} />
           <Route path="commonquestions" element={<CommonQuestion />} />
           <Route path="table" element={<Table />} />
-          <Route path="/graph" element={<Graph />} />
-          <Route path="/uploadfile" element={<UploadFile />} />
+          <Route path="graph" element={<Graph />} />
+          <Route path="uploadfile" element={<UploadFile />} />
         </Route>
       </Routes>
     </Router>
