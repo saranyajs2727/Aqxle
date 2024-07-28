7221393d0e2855508100ec6da7a9a5a85406bc9f
saranyajs2727 <saranyac2727@gmail.com>
2024-07-28 16:54:59 +0530

    data updtaed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@
 import './App.css';
 import GlobalStyle from './common/GlobalStyle';
 import Dashboard from './components/Dashborad/Dashboard';
-import {  HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import {  BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Graph from './components/Graph/Graph';
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    const currentPath = window.location.pathname;
-    if (currentPath !== '/' && currentPath !== '/graph') {
-      window.location = '/#' + currentPath;
-    }
-  }, []);
   return (
     // <div>
     //   <GlobalStyle />
